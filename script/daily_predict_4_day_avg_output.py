1ee87e6e67bef6b63d23b25e3d80d841cd672fc5
李珊珊 <lishanshan@lishanshandeAir.attlocal.net>
2020-08-16 19:31:04 -0700

    define class and function

--- script/daily_predict_4_day_avg_output.py
+++ script/daily_predict_4_day_avg_output.py
@@ -9,20 +9,57 @@ timelagging = 6
 average_num = 4
 feature_num = 16+2*(day_input-1)
 
-"""     read in data and process, this part is very similar to lstm_1.py    """
+class process_data:
+    def __init__(self, input):
+        self.input = input
 
-filename = "daily.csv"
-zipcode_daily = pd.read_csv(filename, encoding="ISO-8859-1", dtype={'ZIP': str, 'date': str})
-zip = zipcode_daily['ZIP']
-date = zipcode_daily['date']  # we have to preserve the date
-del zipcode_daily['ZIP']
-del zipcode_daily['date']
-zipcode_daily = pd.DataFrame(zipcode_daily, dtype=float)  # change the type from 'int' to 'float'
-zipcode_daily['ZIP'] = zip
-zipcode_daily['date'] = date  # add date back
-del zipcode_daily['ave_new6_9after']
-del zipcode_daily['ave_new8_11after']
+    def input_table(self):
+        filename = self.input
+        zipcode_daily = pd.read_csv(filename, encoding="ISO-8859-1", dtype={'ZIP': str, 'date': str})
+        zip = zipcode_daily['ZIP']
+        date = zipcode_daily['date']  # we have to preserve the date
+        del zipcode_daily['ZIP']
+        del zipcode_daily['date']
+        zipcode_daily = pd.DataFrame(zipcode_daily, dtype=float)  # change the type from 'int' to 'float'
+        zipcode_daily['ZIP'] = zip
+        zipcode_daily['date'] = date  # add date back
+        del zipcode_daily['ave_new6_9after']
+        del zipcode_daily['ave_new8_11after']
+        return zipcode_daily
 
+    def model_dist(self):
+        zipcode = []  # save 'zip code' correlating to input point
+        date = []     # save 'date' correlating to input point
+        data_x = []  # input
+        tot = []
+        for key, values in data_dict.items():
+            l = len(values)
+            input_num = l - timelagging - average_num
+            feature = []
+            for i in range(input_num):
+                first = True
+                for j in range(day_input):
+                    if first:
+                        zipcode.append(values[i][-2])   # save 'zip code' correlating to ont input point
+                        date.append(values[i][-1])      # save 'date' correlating to ont input point
+                        # because we add 'date' back, the last feature is values[i][:-5] not values[i][:-4]
+                        for k in values[i][:-3]:
+                            feature.append(k)
+                        first = False
+                    else:
+                        feature.append(values[i + j][0])
+                        feature.append(values[i + j][1])
+                tmp = []
+                tmp.append(feature)
+                data_x.append(tmp)  # one input point
+                feature = []
+        tot.append(zipcode)
+        tot.append(date)
+        tot.append(data_x)
+        return tot
+
+input_file = process_data('daily.csv')
+zipcode_daily = input_file.input_table()
 data_dict = {}  # key: 'zip', value: feature that belong to the key
 for i, zipcode in enumerate(zipcode_daily[:]['ZIP']):
     if zipcode not in data_dict:
@@ -32,32 +69,10 @@ for i, zipcode in enumerate(zipcode_daily[:]['ZIP']):
         feature.append(f)
     data_dict[zipcode].append(feature)
 
-zipcode = []  # save 'zip code' correlating to input point
-date = []     # save 'date' correlating to input point
-
-data_x = []  # input
-for key, values in data_dict.items():
-    l = len(values)
-    input_num = l - timelagging - average_num
-    feature = []
-    for i in range(input_num):
-        first = True
-        for j in range(day_input):
-            if first:
-                zipcode.append(values[i][-2])   # save 'zip code' correlating to ont input point
-                date.append(values[i][-1])      # save 'date' correlating to ont input point
-                # because we add 'date' back, the last feature is values[i][:-5] not values[i][:-4]
-                for k in values[i][:-3]:
-                    feature.append(k)
-                first = False
-            else:
-                feature.append(values[i + j][0])
-                feature.append(values[i + j][1])
-        tmp = []
-        tmp.append(feature)
-        data_x.append(tmp)  # one input point
-        feature = []
-
+total_list = input_file.model_dist()
+zipcode = total_list[0]
+date = total_list[1]
+data_x = total_list[2]
 data_x_ls = []
 for j in data_x:
     for i in j:
@@ -72,7 +87,6 @@ for i in range(len(data_x)):    # using train_x mean and train_x std to normaliz
             data_x[i][j][k] = (data_x[i][j][k] - data_x_mean[k]) / data_x_std[k]
 
 data_x = torch.tensor(data_x)
-
 
 # scale the output value back to its original size and cal the loss
 def upscale(predict):
@@ -108,7 +122,6 @@ model.load_state_dict(checkpoint['net'])
 model.eval()
 predict = np.array(model(data_x.float()).data)  # output
 predict_upscale = upscale(predict)      # original scale output
-
 
 date_list = []
 for i in range(1,32):
@@ -188,3 +201,5 @@ output = out[['date','ZIP','cases/population','risk_level']]
 output.columns = ['Timestamp','Region','Risk Score','Risk Level']
 output = output.sort_values(by=['Timestamp','Region'])
 output.to_csv('daily_predict_4_day_avg_risk.csv',index=False)
+
+
--- script/data_preprocessing.py
+++ script/data_preprocessing.py
@@ -3,182 +3,226 @@ import requests
 import pandas as pd
 import sys
 
+class Community:
+  def __init__(self, url):
+    self.url = url
 
-#google
-Google_Mobility = pd.read_csv('https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv?cachebust=6ec44f00b5b4f6ad')
-Google_Mobility_LA= Google_Mobility[Google_Mobility['sub_region_2']=='Los Angeles County']
-Google_Mobility_LA=Google_Mobility_LA.iloc[:,[7,8,9,10,11,12,13]]
-mask = (Google_Mobility_LA['date'] >= '2020-06-30')
-Google_up_to_date = Google_Mobility_LA.loc[mask]
+  def google_func(self):
+    Google_Mobility = pd.read_csv(self.url)
+    Google_Mobility_LA= Google_Mobility[Google_Mobility['sub_region_2']=='Los Angeles County']
+    Google_Mobility_LA=Google_Mobility_LA.iloc[:,[7,8,9,10,11,12,13]]
+    mask = (Google_Mobility_LA['date'] >= '2020-06-30')
+    Google_up_to_date = Google_Mobility_LA.loc[mask]
+    return Google_up_to_date
 
+  def apple_func(self):
+    CSV_URL = self.url
+    with requests.Session() as s:
+        download = s.get(CSV_URL)
 
-#apple
-CSV_URL = 'https://covid19-static.cdn-apple.com/covid19-mobility-data/2014HotfixDev8/v3/en-us/applemobilitytrends-2020-08-08.csv'
-with requests.Session() as s:
-    download = s.get(CSV_URL)
+        decoded_content = download.content.decode('utf-8')
 
-    decoded_content = download.content.decode('utf-8')
+        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+        my_list = list(cr)
+        tot = []
+        title = my_list[0][6:]
+        for row in my_list:
+            if row[1] == 'Los Angeles':
+                tot.append(row[6:])
+        Apple_Mobility = pd.DataFrame(tot,columns=title)
+        Apple_Mobility_LA = Apple_Mobility.transpose()
+        Apple_Mobility_LA = Apple_Mobility_LA.reset_index()
+        Apple_Mobility_LA.rename(columns={'index':'date',0: 'driving', 1: 'transit', 2: 'walking'}, inplace=True)
+        mask = (Apple_Mobility_LA['date'] >= '2020-06-30')
+        Apple_up_to_date = Apple_Mobility_LA.loc[mask]
+    return Apple_up_to_date
 
-    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-    my_list = list(cr)
-    tot = []
-    title = my_list[0][6:]
-    for row in my_list:
-        if row[1] == 'Los Angeles':
-            tot.append(row[6:])
-    Apple_Mobility = pd.DataFrame(tot,columns=title)
-    Apple_Mobility_LA = Apple_Mobility.transpose()
-    Apple_Mobility_LA = Apple_Mobility_LA.reset_index()
-    Apple_Mobility_LA.rename(columns={'index':'date',0: 'driving', 1: 'transit', 2: 'walking'}, inplace=True)
-    mask = (Apple_Mobility_LA['date'] >= '2020-06-30')
-    Apple_up_to_date = Apple_Mobility_LA.loc[mask]
+  def covid_func(self):
+    CSV_URL_1 = self.url
+    with requests.Session() as s:
+        download = s.get(CSV_URL_1)
 
-total = pd.merge(Apple_up_to_date, Google_up_to_date, on='date')
+        decoded_content = download.content.decode('utf-8')
 
+        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+        my_list = list(cr)
+        title = ['date','county','num','ZIP','confirmed cases']
+        tot = []
+        for row in my_list:
+            if row[1] == 'Los Angeles' and row[0] >= '2020-06-30':
+                tot.append(row[0:5])
+        confirm_num = pd.DataFrame(tot,columns=title)
+        confirm_up_to_date = confirm_num[['date','ZIP','confirmed cases']]
+        target = ['Alhambra', 'Arcadia', 'Beverly Hills', 'Boyle Heights', 'Carson', 'Diamond Bar', 'Encino', 'Gardena',
+                'Glendale', 'Glendora',
+                'Granada Hills', 'Inglewood', 'La Mirada', 'Lancaster', 'Manhattan Beach', 'Melrose', 'Northridge',
+                'San Dimas', 'San Pedro',
+                'Santa Monica', 'Sherman Oaks', 'Silver Lake', 'Tarzana', 'Torrance', 'Venice', 'West Adams',
+                'West Hills', 'West Hollywood',
+                'West Vernon', 'Westchester', 'Altadena', 'Baldwin Hills', 'Brentwood', 'Culver City', 'Eagle Rock',
+                'Hollywood','Hollywood Hills', 'Lynwood', 'Mar Vista', 'Monterey Park', 'North Hollywood', 'Reseda', 'Santa Clarita',
+                'Woodland Hills',
+                'Sylmar', 'Walnut', 'Beverlywood', 'Burbank', 'Calabasas', 'Castaic', 'Covina', 'Crestview',
+                'East Los Angeles', 'Echo Park',
+                'Hancock Park', 'Hawthorne', 'Lawndale', 'Lomita', 'Palms', 'Playa Vista', 'South El Monte',
+                'Stevenson Ranch', 'Studio City',
+                'Tujunga', 'University Park', 'Valley Glen', 'Van Nuys', 'Vermont Knolls', 'Westwood', 'Whittier',
+                'Century City', 'El Segundo',
+                'Lake Balboa', 'Lakewood', 'Miracle Mile', 'Park La Brea', 'Redondo Beach', 'San Fernando',
+                'South Whittier', 'Winnetka',
+                'Del Rey', 'La Canada Flintridge', 'La Verne', 'Montebello', 'Sun Valley', 'Sunland', 'Vermont Vista',
+                'Vernon Central',
+                'West Covina', 'Westlake', 'Bellflower', 'Canoga Park', 'East Hollywood', 'Los Feliz', 'Paramount',
+                'Rancho Palos Verdes',
+                'South Gate', 'Agoura Hills', 'Duarte', 'Exposition Park', 'Hyde Park', 'Lincoln Heights', 'Palmdale',
+                'South Park',
+                'Wilshire Center', 'Canyon Country', 'Claremont', 'Downey', 'Harbor Gateway', 'Harvard Heights',
+                'Highland Park',
+                'La Puente', 'Norwalk', 'Pico Rivera', 'Porter Ranch', 'San Gabriel', 'Wholesale District', 'Willowbrook',
+                'Arleta',
+                'Bell Gardens', 'Glassell Park', 'Panorama City', 'Pomona', 'Valinda', 'Watts', 'Azusa', 'Bell',
+                'Chatsworth',
+                'Hacienda Heights', 'Harbor City', 'Leimert Park', 'Maywood', 'Monrovia', 'North Hills', 'Pacoima',
+                'Avalon', 'Baldwin Park',
+                'Bassett', 'Central', 'El Monte', 'El Sereno', 'Harvard Park', 'Lake Los Angeles', 'Rosemead',
+                'Rowland Heights', 'Temple City',
+                'Acton', 'Cerritos', 'Cloverdale/Cochran', 'Compton', 'Downtown', 'Huntington Park', 'Koreatown',
+                'Mt. Washington', 'Pasadena',
+                'South Pasadena', 'Wilmington']
 
-
-#covid19 confirmed cases
-CSV_URL = "https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/latimes-place-totals.csv"
-with requests.Session() as s:
-    download = s.get(CSV_URL)
-
-    decoded_content = download.content.decode('utf-8')
-
-    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-    my_list = list(cr)
-    title = ['date','county','num','ZIP','confirmed cases']
-    tot = []
-    for row in my_list:
-        if row[1] == 'Los Angeles' and row[0] >= '2020-06-30':
-            tot.append(row[0:5])
-    confirm_num = pd.DataFrame(tot,columns=title)
-    confirm_up_to_date = confirm_num[['date','ZIP','confirmed cases']]
-    target = ['Alhambra', 'Arcadia', 'Beverly Hills', 'Boyle Heights', 'Carson', 'Diamond Bar', 'Encino', 'Gardena',
-              'Glendale', 'Glendora',
-              'Granada Hills', 'Inglewood', 'La Mirada', 'Lancaster', 'Manhattan Beach', 'Melrose', 'Northridge',
-              'San Dimas', 'San Pedro',
-              'Santa Monica', 'Sherman Oaks', 'Silver Lake', 'Tarzana', 'Torrance', 'Venice', 'West Adams',
-              'West Hills', 'West Hollywood',
-              'West Vernon', 'Westchester', 'Altadena', 'Baldwin Hills', 'Brentwood', 'Culver City', 'Eagle Rock',
-              'Hollywood',
-              'Hollywood Hills', 'Lynwood', 'Mar Vista', 'Monterey Park', 'North Hollywood', 'Reseda', 'Santa Clarita',
-              'Woodland Hills',
-              'Sylmar', 'Walnut', 'Beverlywood', 'Burbank', 'Calabasas', 'Castaic', 'Covina', 'Crestview',
-              'East Los Angeles', 'Echo Park',
-              'Hancock Park', 'Hawthorne', 'Lawndale', 'Lomita', 'Palms', 'Playa Vista', 'South El Monte',
-              'Stevenson Ranch', 'Studio City',
-              'Tujunga', 'University Park', 'Valley Glen', 'Van Nuys', 'Vermont Knolls', 'Westwood', 'Whittier',
-              'Century City', 'El Segundo',
-              'Lake Balboa', 'Lakewood', 'Miracle Mile', 'Park La Brea', 'Redondo Beach', 'San Fernando',
-              'South Whittier', 'Winnetka',
-              'Del Rey', 'La Canada Flintridge', 'La Verne', 'Montebello', 'Sun Valley', 'Sunland', 'Vermont Vista',
-              'Vernon Central',
-              'West Covina', 'Westlake', 'Bellflower', 'Canoga Park', 'East Hollywood', 'Los Feliz', 'Paramount',
-              'Rancho Palos Verdes',
-              'South Gate', 'Agoura Hills', 'Duarte', 'Exposition Park', 'Hyde Park', 'Lincoln Heights', 'Palmdale',
-              'South Park',
-              'Wilshire Center', 'Canyon Country', 'Claremont', 'Downey', 'Harbor Gateway', 'Harvard Heights',
-              'Highland Park',
-              'La Puente', 'Norwalk', 'Pico Rivera', 'Porter Ranch', 'San Gabriel', 'Wholesale District', 'Willowbrook',
-              'Arleta',
-              'Bell Gardens', 'Glassell Park', 'Panorama City', 'Pomona', 'Valinda', 'Watts', 'Azusa', 'Bell',
-              'Chatsworth',
-              'Hacienda Heights', 'Harbor City', 'Leimert Park', 'Maywood', 'Monrovia', 'North Hills', 'Pacoima',
-              'Avalon', 'Baldwin Park',
-              'Bassett', 'Central', 'El Monte', 'El Sereno', 'Harvard Park', 'Lake Los Angeles', 'Rosemead',
-              'Rowland Heights', 'Temple City',
-              'Acton', 'Cerritos', 'Cloverdale/Cochran', 'Compton', 'Downtown', 'Huntington Park', 'Koreatown',
-              'Mt. Washington', 'Pasadena',
-              'South Pasadena', 'Wilmington']
-
-    confirm_up_to_date = confirm_up_to_date[confirm_up_to_date['ZIP'].isin(target)]
-    confirm_up_to_date = confirm_up_to_date.sort_values(by=['ZIP','date'])
-    place_dict = {}
-    for index in range(confirm_up_to_date.shape[0]):
-        key = confirm_up_to_date.iloc[index,1]
-        if key not in place_dict:
-            place_dict[key] = []
-            place_dict[key].append(confirm_up_to_date.iloc[index,2])
-        else:
-            place_dict[key].append(confirm_up_to_date.iloc[index, 2])
-    new = []
-    for key in place_dict:
-        value = place_dict[key]
-        count = 0
-        for i in value:
-            if count ==0:
-                new.append(10000)
-                count += 1
+        confirm_up_to_date = confirm_up_to_date[confirm_up_to_date['ZIP'].isin(target)]
+        confirm_up_to_date = confirm_up_to_date.sort_values(by=['ZIP','date'])
+        place_dict = {}
+        for index in range(confirm_up_to_date.shape[0]):
+            key = confirm_up_to_date.iloc[index,1]
+            if key not in place_dict:
+                place_dict[key] = []
+                place_dict[key].append(confirm_up_to_date.iloc[index,2])
             else:
-                num = int(i) - int(value[count-1])
-                new.append(num)
+                place_dict[key].append(confirm_up_to_date.iloc[index, 2])
+        new = []
+        for key in place_dict:
+            value = place_dict[key]
+            count = 0
+            for i in value:
+                if count ==0:
+                    new.append(10000)
+                    count += 1
+                else:
+                    num = int(i) - int(value[count-1])
+                    new.append(num)
+                    count += 1
+        confirm_up_to_date['new_confirmed_cases'] = new
+        mask = (confirm_up_to_date['new_confirmed_cases'] != 10000)
+        confirm_up_to_date = confirm_up_to_date.loc[mask]
+        confirm_up_to_date = confirm_up_to_date.sort_values(by=['date','ZIP'])
+    return confirm_up_to_date
+
+  def population_func(self):
+    population = pd.read_csv(self.url)
+    population = population.sort_values(by=['place'])
+    population = population[['place','population','Population.Density','incomLev']]
+    population.columns = ['ZIP','population','Population.Density','incomLev']
+    return population
+
+def combine_table(t1,t2,column):
+    table = pd.merge(t1,t2, on=column)
+    return table
+
+google_data = Community('https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv?cachebust=6ec44f00b5b4f6ad')
+google = google_data.google_func()
+
+apple_data = Community('https://covid19-static.cdn-apple.com/covid19-mobility-data/2014HotfixDev8/v3/en-us/applemobilitytrends-2020-08-08.csv')
+apple = apple_data.apple_func()
+total_table = combine_table(apple, google,'date')
+
+confirm_data = Community('https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/latimes-place-totals.csv')
+confirm = confirm_data.covid_func()
+total_table = combine_table(confirm,total_table,'date')
+
+population_data = Community('population_income.csv')
+popul = population_data.population_func()
+total_table = combine_table(total_table, popul,'ZIP')
+
+class update:
+    def __init__(self, table):
+        self.table = table
+
+    def new_case(self):
+        total = self.table
+        case_adjust = []
+        new_case_adjust = []
+        tot = []
+        for index in range(total.shape[0]):
+            case_num = int(total.iloc[index,2]) / int(total.iloc[index,13])
+            new_case_num = int(total.iloc[index,3]) / int(total.iloc[index,13])
+            case_adjust.append(case_num)
+            new_case_adjust.append(new_case_num)
+        tot.append(case_adjust)
+        tot.append(new_case_adjust)
+        return tot
+
+    def future_case(self):
+        total = self.table
+        ave_dict = {}
+        tot_list = []
+        new6 = []
+        new7 = []
+        new8 = []
+        for index in range(total.shape[0]):
+            key = total.iloc[index, 1]
+            if key not in ave_dict:
+                ave_dict[key] = []
+                ave_dict[key].append(total.iloc[index, 3])
+            else:
+                ave_dict[key].append(total.iloc[index, 3])
+        for k in ave_dict:
+            value = ave_dict[k]
+            count = 0
+            for i in value:
+                if count + 11 <= len(value) - 1:
+                    num6 = (value[count + 6] + value[count + 7] + value[count + 8] + value[count + 9]) / 4
+                    new6.append(num6)
+                    num7 = (value[count + 7] + value[count + 8] + value[count + 9] + value[count + 10]) / 4
+                    new7.append(num7)
+                    num8 = (value[count + 8] + value[count + 9] + value[count + 10] + value[count + 11]) / 4
+                    new8.append(num8)
+                elif count + 10 <= len(value) - 1:
+                    num6 = (value[count + 6] + value[count + 7] + value[count + 8] + value[count + 9]) / 4
+                    new6.append(num6)
+                    num7 = (value[count + 7] + value[count + 8] + value[count + 9] + value[count + 10]) / 4
+                    new7.append(num7)
+                    new8.append(0)
+                elif count + 9 <= len(value) - 1:
+                    num6 = (value[count + 6] + value[count + 7] + value[count + 8] + value[count + 9]) / 4
+                    new6.append(num6)
+                    new7.append(0)
+                    new8.append(0)
+                else:
+                    new6.append(0)
+                    new7.append(0)
+                    new8.append(0)
                 count += 1
-    confirm_up_to_date['new_confirmed_cases'] = new
-    mask = (confirm_up_to_date['new_confirmed_cases'] != 10000)
-    confirm_up_to_date = confirm_up_to_date.loc[mask]
-    confirm_up_to_date = confirm_up_to_date.sort_values(by=['date','ZIP'])
-    total = pd.merge(confirm_up_to_date, total, on='date')
+        tot_list.append(new6)
+        tot_list.append(new7)
+        tot_list.append(new8)
+        return tot_list
+
+case_column = update(total_table)
+case_list = case_column.new_case()
+case_adjust = case_list[0]
+new_case_adjust = case_list[1]
+total_table['cases_adjusted_by_pop'] = case_adjust
+total_table['new_cases_adjusted_by_pop'] = new_case_adjust
+
+future_list = case_column.future_case()
+total_table['ave_new6_9after'] = future_list[0]
+total_table['ave_new7_10after'] = future_list[1]
+total_table['ave_new8_11after'] = future_list[2]
+total_table.to_csv('daily.csv',index=False)
 
 
-#population and income
-population = pd.read_csv('population_income.csv')
-population = population.sort_values(by=['place'])
-population = population[['place','population','Population.Density','incomLev']]
-population.columns = ['ZIP','population','Population.Density','incomLev']
-total = pd.merge(total, population, on='ZIP')
 
 
-case_adjust = []
-new_case_adjust = []
-for index in range(total.shape[0]):
-    case_num = int(total.iloc[index,2]) / int(total.iloc[index,13])
-    new_case_num = int(total.iloc[index,3]) / int(total.iloc[index,13])
-    case_adjust.append(case_num)
-    new_case_adjust.append(new_case_num)
-total['cases_adjusted_by_pop'] = case_adjust
-total['new_cases_adjusted_by_pop'] = new_case_adjust
 
-#ave_new7_10after,ave_new6_9after,ave_new8_11after column
-ave_dict = {}
-new6 = []
-new7 = []
-new8 = []
-for index in range(total.shape[0]):
-    key = total.iloc[index, 1]
-    if key not in ave_dict:
-        ave_dict[key] = []
-        ave_dict[key].append(total.iloc[index, 3])
-    else:
-        ave_dict[key].append(total.iloc[index, 3])
-for k in ave_dict:
-    value = ave_dict[k]
-    count = 0
-    for i in value:
-        if count + 11 <= len(value) - 1:
-            num6 = (value[count + 6] + value[count + 7] + value[count + 8] + value[count + 9]) / 4
-            new6.append(num6)
-            num7 = (value[count + 7] + value[count + 8] + value[count + 9] + value[count + 10]) / 4
-            new7.append(num7)
-            num8 = (value[count + 8] + value[count + 9] + value[count + 10] + value[count + 11]) / 4
-            new8.append(num8)
-        elif count + 10 <= len(value) - 1:
-            num6 = (value[count + 6] + value[count + 7] + value[count + 8] + value[count + 9]) / 4
-            new6.append(num6)
-            num7 = (value[count + 7] + value[count + 8] + value[count + 9] + value[count + 10]) / 4
-            new7.append(num7)
-            new8.append(0)
-        elif count + 9 <= len(value) - 1:
-            num6 = (value[count + 6] + value[count + 7] + value[count + 8] + value[count + 9]) / 4
-            new6.append(num6)
-            new7.append(0)
-            new8.append(0)
-        else:
-            new6.append(0)
-            new7.append(0)
-            new8.append(0)
-        count += 1
 
-total['ave_new6_9after'] = new6
-total['ave_new7_10after'] = new7
-total['ave_new8_11after'] = new8
-total.to_csv('daily.csv',index=False)
